3c204882e0feeb3b92656f137bc17f6f0a9831ca
harpercham <43534952+harpercham@users.noreply.github.com>
2018-11-21 21:38:11 +0800

    Update calculate.js

--- calculate.js
+++ calculate.js
@@ -67,3 +67,17 @@ function calculate() {
     document.getElementById("priceJumlah").innerHTML = " RM " + priceJumlah.toFixed();
 
 };
+var x = document.getElementById("demo");
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude;
+}
