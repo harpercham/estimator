98ee44f328c221d06db8115e482f98a51404cac5
harpercham <43534952+harpercham@users.noreply.github.com>
2018-11-26 18:05:47 +0800

    Update calculate.js

--- calculate.js
+++ calculate.js
@@ -72,14 +72,6 @@ function calculate() {
 var x = document.getElementById("demo");
 
 function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else { 
-        x.innerHTML = "Geolocation is not supported by this browser.";
-    }
-}
-
-function getLocation() {
 setInterval(function () {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
